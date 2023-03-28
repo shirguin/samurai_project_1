24d3c426a4cfe88bc054663c46ece0cdcc75c5c9
shirguin <shirguin@yandex.ru>
2023-03-28 07:58:47 +0300

    start project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <img src="https://w7.pngwing.com/pngs/464/586/png-transparent-thor-clint-barton-logo-hulk-decal-logo-shield-marvel-avengers-assemble-text-trademark.png"></img>
       </header>
+      <nav>
+        <div>Profile</div>
+      </nav>
     </div>
   );
-}
+};
 
 export default App;
