3873ea5ed58c39659dbabe9a6528121a270a4c51
shirguin <shirguin@yandex.ru>
2023-03-28 11:46:14 +0300

    start Lesson_12

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header className="header">
+      <img src="https://i.pinimg.com/originals/50/65/f9/5065f93bde11bf9ff4b21cf4c50eeab8.png" />
+    </header>
+  );
+};
+
+export default Navbar;
