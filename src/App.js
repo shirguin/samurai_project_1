28dbedc7a92aa8e60dd008d2f10b674dc93a1e18
shirguin <shirguin@yandex.ru>
2023-05-03 15:48:26 +0300

    END Lesson_26

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-const App = () => {
+const App = (props) => {
+
   return (
     <BrowserRouter>
       <div className="app-wrapper">
@@ -17,8 +18,8 @@ const App = () => {
         <Navbar />
         <div className="app-wrapper-content">
           <Routes>
-            <Route path="/dialogs" element={<Dialogs />} />
-            <Route path="/profile" element={<Profile />} />
+            <Route path="/dialogs" element={<Dialogs dialogs = {props.dialogs} messages = {props.messages}/>} />
+            <Route path="/profile" element={<Profile posts = {props.posts}/>} />
             <Route path="/news" element={<News />} />
             <Route path="/music" element={<Music />} />
             <Route path="/settings" element={<Settings />} />
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,43 +1,21 @@
 import React from "react";
 import classes from "./Dialogs.module.css";
-import { NavLink } from "react-router-dom";
-
-const DialogItem = (props) => {
-  let path = "/dialogs/" + props.id;
-
-  return (
-    <div className={classes.dialog + " " + classes.active}>
-      <NavLink to={path}>{props.name}</NavLink>
-    </div>
-  );
-};
-
-const Message = (props)=>{
-  return(
-    <div className={classes.message}>{props.message}</div>
-  )
-}
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message";
 
 const Dialogs = (props) => {
+
+  const dialogsElements = props.dialogs.map((dialog) => (
+    <DialogItem name={dialog.name} id={dialog.id} />
+  ));
+  const messagesElements = props.messages.map((message) => (
+    <Message message={message.message} />
+  ));
+
   return (
     <div className={classes.dialogs}>
-      <div className={classes.dialogsItems}>
-        <DialogItem name="Алексей" id="1" />
-        <DialogItem name="Иван" id="2" />
-        <DialogItem name="Светлана" id="3" />
-        <DialogItem name="Марина" id="4" />
-        <DialogItem name="Петр" id="5" />
-        <DialogItem name="Сергей" id="6" />
-        <DialogItem name="Андрей" id="7" />
-      </div>
-      <div className={classes.messages}>
-        <Message message = 'Привет!'/>
-        <Message message = 'Как дела?'/>
-        <Message message = 'Все ок'/>
-        <Message message = 'Прикольно'/>
-        <Message message = 'Как сам?'/>
-        <Message message = 'Что на завтра?'/>
-      </div>
+      <div className={classes.dialogsItems}>{dialogsElements}</div>
+      <div className={classes.messages}>{messagesElements}</div>
     </div>
   );
 };
